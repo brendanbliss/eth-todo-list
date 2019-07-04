fe1705b8e769f9214800dcf7f5d210b1b19d6800
brendanbliss <brendanmbliss@gmail.com>
2019-07-04 15:42:55 -0600

    create task client side

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -11,6 +11,12 @@ contract TodoList {
 
     mapping(uint => Task) public tasks;
 
+    event TaskCreated(
+        uint id,
+        string content,
+        bool completed
+    );
+
     constructor() public {
         createTask("Check 123");
     }
@@ -18,7 +24,8 @@ contract TodoList {
     function createTask(string memory _content) public {
         taskCount ++;
         tasks[taskCount] = Task(taskCount, _content, false);
+        emit TaskCreated(taskCount, _content, false);
     }
 
-    
+
 }
